2052b487cb92389d5023d5028c4d4a019a48dbf5
ethan <ethangray909@gmail.com>
2022-09-30 13:20:07 +0500

    added options dialog

--- index.ts
+++ index.ts
@@ -1,40 +1,96 @@
-import { NativeModules, Platform } from 'react-native';
-import { ImagePickerOptions } from 'rn-image-picker/types';
 
+import { Alert, InteractionManager, NativeModules, Platform } from 'react-native';
+import { ImagePickerOptions } from 'rn-image-picker/types';
 const { imagePicker } = NativeModules;
 
+
+
 const open = async (options: ImagePickerOptions) => {
-    return new Promise((resolve, reject) => {
-        if (imagePicker) {
-            if (imagePicker?.openImagePicker) {
-                try {
-                    if (Platform.OS == "android") {
-                        imagePicker.openImagePicker(options, response => {
-                            console.log(response);
-                            resolve(response)
-                        })
 
+    return new Promise(async (resolve, reject) => {
 
-                    } else if (Platform.OS == "ios") {
-                        let response = imagePicker.openImagePicker(options)
-                        resolve(response)
-                    }
+        try {
 
-                    // return resolve(response)
-                } catch (e) {
-                    reject(e)
+            if (!imagePicker || !imagePicker?.openImagePicker) throw new Error('Something went wrong')
 
-                    // return Promise.reject(e)
-                }
+            if (Platform.OS == "android") {
+
+                imagePicker.openImagePicker(options, response => {
+
+                    resolve(response)
+
+                })
+
+            } else {
+
+                let response = await imagePicker.openImagePicker(options)
+
+                resolve(response)
+
             }
+
+        } catch (error) {
+
+            reject(error)
+
         }
+
     })
 
+}
 
+export const openPicker = async (options: ImagePickerOptions) => {
 
+    return new Promise((resolve, reject) => {
+
+        Alert.alert('Image Picker', 'Please select your desired source', [
+
+            {
+                text: 'Cancel',
+                style: 'destructive',
+                onPress: () => InteractionManager.runAfterInteractions(() => {
+                    reject('User cancelled')
+                })
+
+            },
+
+            {
+
+                text: 'Camera',
+                onPress: () => InteractionManager.runAfterInteractions(async () => {
+                    try {
+                        const response = await open({ ...options, selection: 'camera' })
+                        resolve(response)
+                    } catch (error) {
+                        reject(error)
+
+                    }
+                })
+            },
+
+            {
+                text: 'Gallery',
+                onPress: () => InteractionManager.runAfterInteractions(async () => {
+                    try {
+                        const response = await open({ ...options, selection: 'gallery' })
+                        resolve(response)
+                    } catch (error) {
+                        reject(error)
+                    }
+                })
+            },
+        ], {
+            cancelable: false,
+        })
+
+    })
 
 }
 
 export default {
-    open: open
+
+    open: open,
+
+    openPicker: openPicker
+
 };
